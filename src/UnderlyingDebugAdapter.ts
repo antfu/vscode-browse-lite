192d425bb96c7694c59c68cd337a3b121e024d92
Anthony Fu <anthonyfu117@hotmail.com>
2021-09-13 09:10:09 +0800

    fix: temporary workaround for debugger warning

--- src/UnderlyingDebugAdapter.ts
+++ src/UnderlyingDebugAdapter.ts
@@ -1,11 +1,10 @@
-import { extensions, window } from 'vscode'
+import { extensions } from 'vscode'
 
 export function getUnderlyingDebugType(): string {
-    if (!!extensions.getExtension('msjsdiag.debugger-for-chrome')) {
-        return 'chrome'
-    }
-    if (!!extensions.getExtension('msjsdiag.debugger-for-edge')) {
-        return 'edge'
-    }
-    window.showErrorMessage('Install "Debugger for Chrome" or "Debugger for Microsoft Edge" and Reload Window required.')
+  if (extensions.getExtension('msjsdiag.debugger-for-chrome'))
+    return 'chrome'
+  if (extensions.getExtension('msjsdiag.debugger-for-edge'))
+    return 'edge'
+  // TODO: support VS Code built-in debugger
+  return 'chrome'
 }
